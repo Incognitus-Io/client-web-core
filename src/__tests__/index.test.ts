74547e97fb10dca789e951ad69ecedaf13745539
Jamie Stumme <jamesstumme@gmail.com>
2020-12-13 19:09:49 -0700

    ✨ feat(service): implement incognitus service

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,249 @@
-import foo from '../index';
+import fetchMock from 'jest-fetch-mock';
 
-describe('Entrypoint', () => {
-  it('should return foo', () => {
-    expect(foo).toBeDefined();
+import { IncognitusService, IncognitusConfig } from '../index';
+
+describe('Incognitus Service', () => {
+  let config: IncognitusConfig;
+
+  beforeEach(() => {
+    fetchMock.resetMocks();
+    IncognitusService.reset();
+    fetchMock.mockResponse(async () => ({
+      body: JSON.stringify({ Features: [] }),
+    }));
+    config = {
+      aipUrl: 'https://localhost',
+      tenantId: 'abc',
+      applicationId: 'xyz.123',
+    };
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('instance', () => {
+    it('should throw error when getting instance before initializing', async () => {
+      expect(() => IncognitusService.instance).toThrowError();
+    });
+
+    it('should return instance after initializing', async () => {
+      await IncognitusService.initialize({
+        tenantId: 'abc',
+        applicationId: 'xyz.123',
+      } as IncognitusConfig);
+      expect(IncognitusService.instance).toBeDefined();
+    });
+  });
+
+  describe('initialize', () => {
+    it('should create a new instance every time it`s called', async () => {
+      await IncognitusService.initialize(config);
+      const instance1 = IncognitusService.instance;
+      await IncognitusService.initialize(config);
+      const instance2 = IncognitusService.instance;
+
+      expect(instance1).not.toBe(instance2);
+    });
+
+    it('should fetch the initial set of features', async () => {
+      fetchMock.mockResponse(async (req) => {
+        if (req.url.endsWith('/feature')) {
+          return {
+            body: JSON.stringify({
+              Features: [
+                { name: 'feat1', isEnabled: true },
+                { name: 'feat2', isEnabled: true },
+              ],
+            }),
+            status: 200,
+          };
+        }
+
+        return {
+          status: 500,
+        };
+      });
+
+      await IncognitusService.initialize(config);
+
+      const instance = IncognitusService.instance;
+      await expect(instance.isEnabled('feat1')).resolves.toBe(true);
+      await expect(instance.isEnabled('feat2')).resolves.toBe(true);
+    });
+  });
+
+  describe('ctor', () => {
+    it('should throw an error when the tenant is blank', () => {
+      expect(
+        () =>
+          new IncognitusService({
+            applicationId: 'abc',
+          } as IncognitusConfig),
+      ).toThrowError();
+    });
+
+    it('should throw an error when the application is blank', () => {
+      expect(
+        () =>
+          new IncognitusService({
+            tenantId: 'abc',
+          } as IncognitusConfig),
+      ).toThrowError();
+    });
+  });
+
+  describe('isEnabled', () => {
+    it('should return the status when feature is cached', async () => {
+      const svc = new IncognitusService(config);
+      svc.setCache(
+        new Map([
+          ['foo', true],
+          ['bar', false],
+        ]),
+      );
+
+      await expect(svc.isEnabled('foo')).resolves.toBe(true);
+      await expect(svc.isEnabled('bar')).resolves.toBe(false);
+    });
+
+    it('should fetch missing flags', async () => {
+      fetchMock
+        .mockOnce(JSON.stringify({ name: 'foo', isEnabled: true }))
+        .mockReject();
+
+      const svc = new IncognitusService(config);
+      await svc.isEnabled('foo');
+
+      expect(fetchMock).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('isDisabled', () => {
+    it('should return the status when feature is cached', async () => {
+      const svc = new IncognitusService(config);
+      svc.setCache(
+        new Map([
+          ['foo', false],
+          ['bar', true],
+        ]),
+      );
+
+      await expect(svc.isDisabled('foo')).resolves.toBe(true);
+      await expect(svc.isDisabled('bar')).resolves.toBe(false);
+    });
+
+    it('should fetch missing flags', async () => {
+      fetchMock
+        .mockOnce(JSON.stringify({ name: 'foo', isEnabled: true }))
+        .mockReject();
+
+      const svc = new IncognitusService(config);
+      await svc.isDisabled('foo');
+
+      expect(fetchMock).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getAllFeatures', () => {
+    let svc: IncognitusService;
+
+    beforeEach(() => {
+      svc = new IncognitusService(config);
+    });
+
+    it('should fetch all features', async () => {
+      await svc.getAllFeatures();
+
+      expect(fetchMock).toBeCalledWith(`${config.aipUrl}/feature`, {
+        headers: {
+          'X-Application': config.applicationId,
+          'X-Tenant': config.tenantId,
+        },
+      });
+    });
+
+    it('should log an error when api fails', async () => {
+      fetchMock.mockResponse(async () => ({ status: 500 }));
+      const spy = jest.spyOn(console, 'error').mockImplementation(() => {
+        /* nop */
+      });
+
+      await svc.getAllFeatures();
+
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it('should log an error when fetch fails', async () => {
+      fetchMock.mockReject();
+      const spy = jest.spyOn(console, 'error').mockImplementation(() => {
+        /* nop */
+      });
+
+      await svc.getAllFeatures();
+
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it('should set the cache', async () => {
+      fetchMock
+        .mockOnce(
+          JSON.stringify({ Features: [{ name: 'foo', isEnabled: true }] }),
+        )
+        .mockReject();
+
+      await svc.getAllFeatures();
+      const res = await svc.isEnabled('foo');
+      expect(res).toBe(true);
+    });
+  });
+
+  describe('getFeature', () => {
+    let svc: IncognitusService;
+
+    beforeEach(() => {
+      svc = new IncognitusService(config);
+    });
+
+    it('should log an error when api fails', async () => {
+      fetchMock.mockResponse(async () => ({ status: 500 }));
+      const spy = jest.spyOn(console, 'error').mockImplementation(() => {
+        /* nop */
+      });
+
+      const res = await svc.getFeature('');
+
+      expect(spy).toHaveBeenCalled();
+      expect(res).toBe(false);
+    });
+
+    it('should log an error when fetch fails', async () => {
+      fetchMock.mockReject();
+      const spy = jest.spyOn(console, 'error').mockImplementation(() => {
+        /* nop */
+      });
+
+      const res = await svc.getFeature('');
+
+      expect(spy).toHaveBeenCalled();
+      expect(res).toBe(false);
+    });
+
+    it('should return the status of the feature', async () => {
+      fetchMock.mockOnce(JSON.stringify({ name: 'foo', isEnabled: true }));
+
+      const res = await svc.getFeature('foo');
+      expect(res).toBe(true);
+    });
+
+    it('should set the cache', async () => {
+      fetchMock
+        .mockOnce(JSON.stringify({ name: 'foo', isEnabled: true }))
+        .mockReject();
+
+      await svc.getFeature('foo');
+      const res = await svc.isEnabled('foo');
+      expect(res).toBe(true);
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,150 @@
-export default 'foo';
+export interface IncognitusConfig {
+  aipUrl?: string;
+  tenantId: string;
+  applicationId: string;
+}
+
+interface AllFeatureResponse {
+  Features: Feature[];
+}
+
+interface Feature {
+  name: string;
+  isEnabled: boolean;
+}
+
+interface ApiHeaders {
+  'X-Tenant': string;
+  'X-Application': string;
+  [key: string]: string;
+}
+
+/**
+ * A service for communicating with the Incognitus API.
+ */
+export class IncognitusService {
+  /* test-code */
+  public setCache(cache: Map<string, boolean>) {
+    this.featureCache = Object.freeze(cache);
+  }
+  public static reset() {
+    IncognitusService._instance = undefined;
+  }
+  /* end-test-code */
+
+  /**
+   * @returns the instance of the Incognitus service.
+   */
+  public static get instance() {
+    if (!IncognitusService._instance) {
+      throw new Error('Service not initialized');
+    }
+    return IncognitusService._instance;
+  }
+  private static _instance?: IncognitusService;
+
+  /**
+   * Initalizes the Incognitus service and fetches all features.
+   *
+   * @param config the configuration options.
+   */
+  public static initialize = async (config: IncognitusConfig) => {
+    const service = new IncognitusService(config);
+    await service.getAllFeatures();
+    IncognitusService._instance = service;
+  };
+
+  private featureCache = Object.freeze(new Map<string, boolean>());
+  private fetchHeaders: ApiHeaders;
+
+  /**
+   * @internal Please use `IncognitusService.instance` instead.
+   */
+  constructor(private config: IncognitusConfig) {
+    if (!config.tenantId) throw new Error('`tenantId` is required');
+    if (!config.applicationId) throw new Error('`applicationId` is required');
+    this.fetchHeaders = {
+      'X-Tenant': config.tenantId,
+      'X-Application': config.applicationId,
+    } as ApiHeaders;
+  }
+
+  private get baseUri() {
+    const base = this.config.aipUrl || 'https://incognitus.io/api';
+    return `${base}/feature`;
+  }
+
+  /**
+   * Checks if a feature flag is enabled.  If not previously fetched, this
+   * will also fetch the flag.
+   * @param name The name of the feature flag.
+   */
+  public async isEnabled(name: string) {
+    return this.featureCache.get(name) ?? (await this.getFeature(name));
+  }
+
+  /**
+   * Checks if a feature flag is disabled.  If not previously fetched, this
+   * will also fetch the flag.
+   * @param name The name of the feature flag.
+   */
+  public async isDisabled(name: string) {
+    return !(await this.isEnabled(name));
+  }
+
+  /**
+   * Fetches all feature flags for the configured service.
+   */
+  public async getAllFeatures() {
+    try {
+      const res = await fetch(this.baseUri, {
+        headers: this.fetchHeaders,
+      });
+
+      if (!res.ok) {
+        console.error('Failed to get all features');
+        return;
+      }
+      const apiRes = (await res.json()) as AllFeatureResponse;
+      const features = apiRes.Features.reduce(
+        (acc, val) => acc.set(val.name, val.isEnabled),
+        new Map<string, boolean>(),
+      );
+      this.featureCache = Object.freeze(features);
+    } catch (e) {
+      console.error('Failed to get all features', e);
+    }
+  }
+
+  /**
+   * Fetches a specified feature flag
+   *
+   * @param name The name of the feature flag to fetch.
+   * @returns The status of the feature flag, else `False` if
+   * the request failed.
+   */
+  public async getFeature(name: string) {
+    try {
+      const res = await fetch(`${this.baseUri}/${name}`, {
+        headers: this.fetchHeaders,
+      });
+
+      if (!res.ok) {
+        console.error('Failed to get feature');
+        return false;
+      }
+      const apiRes = (await res.json()) as Feature;
+
+      const features = new Map<string, boolean>(
+        this.featureCache as Map<string, boolean>,
+      );
+      features.set(apiRes.name, apiRes.isEnabled);
+      this.featureCache = Object.freeze(features);
+
+      return apiRes.isEnabled;
+    } catch (e) {
+      console.error('Failed to get feature', e);
+      return false;
+    }
+  }
+}
